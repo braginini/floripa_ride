c2a966c9f3c8b8364608a7a01318fbfc1b7bfd23
mikhail.bragin <mikhail@grupoxago.com>
2013-07-29 19:35:27 -0300

    init prj added routes xmls

---
+++ osm-parser/src/main/java/com/floriparide/osmparser/OsmParserLauncher.java
@@ -0,0 +1,12 @@
+package com.floriparide.osmparser;
+
+/**
+ * Created by Mikhail Bragin
+ */
+public class OsmParserLauncher {
+
+	public static void main(String[] args) throws Exception {
+
+
+	}
+}
